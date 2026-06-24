374f3507768d223ae1178f45476ed48abc044754
Sudhanshu Roy <sudhanshup434@gmail.com>
2026-06-24 22:58:19 +0530

    Update news_service.py

--- backend/news_service.py
+++ backend/news_service.py
@@ -1,45 +1,32 @@
-import os
-import requests
-
-FINNHUB_API_KEY = os.getenv(
-    "FINNHUB_API_KEY"
-)
+from yahooquery import search
 
 
-def get_news(
-    ticker
-):
+def get_news(ticker):
 
-    url = (
-        f"https://finnhub.io/api/v1/company-news"
-        f"?symbol={ticker}"
-        f"&from=2025-01-01"
-        f"&to=2026-12-31"
-        f"&token={FINNHUB_API_KEY}"
+    results = search(
+        ticker
     )
 
-    data = (
-        requests
-        .get(url)
-        .json()
+    news = (
+        results
+        .get("news", [])
     )
 
     articles = []
 
-    for article in data[:5]:
+    for article in news[:5]:
 
         articles.append({
 
             "title":
-            article.get(
-                "headline"
-            ),
+            article.get("title"),
 
             "publisher":
             article.get(
-                "source"
+                "publisher"
             )
 
         })
 
     return articles
+
